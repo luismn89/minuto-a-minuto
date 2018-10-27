6ff718d3914f51c355f85d847e70cf5cbfd2f5a5
luismn89 <luismn89@gmail.com>
2018-10-26 21:40:28 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/minamin-component.js',
+  output: {
+    path: path.resolve(__dirname, '../../static/especial/js/'),
+    publicPath: '/dist/',
+    filename: 'component-minamin.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+          }
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true,
+    proxy: {
+      "/mundial/especial/api/v1": "http://polla.pytel.pe",
+      "/mundial/especial/dist/polla": {
+        target: 'http://localhost:8080/dist',
+        pathRewrite: { '^/mundial/polla/dist/polla': '' }
+      }
+    }
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
